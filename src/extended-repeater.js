87e3300b44dda97c0ea382b310b88fc2ca0cccd4
Egor Nedelko <gojiramedia@gmail.com>
2021-01-19 11:08:42 +0300

    refactor: rework extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,26 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(str, options) {
-  let strArr = []
-  let addArr = []
-
-  if (options.separator == undefined) options.separator = '+'
-  if (options.additionSeparator == undefined) options.additionSeparator = '|'
-
-  for (let i = 0; i < options.repeatTimes; i++) {
-    strArr.push(str)
-  }
-
-  for (let i = 0; i < options.additionRepeatTimes; i++) {
-    addArr.push(options.addition)
-  }
-
-  const addendum = options.additionSeparator + addArr.join(options.additionSeparator)
-
-  for (item in strArr) {
-    item += addendum
-  }
-
-  return strArr.join(options.separator)
+module.exports = function repeater(str, { repeatTimes = 1, separator = '+', addition = '', additionRepeatTimes = 1, additionSeparator = '|' } ) {
+  let additionString = new Array(additionRepeatTimes).fill(`${addition}`).join(additionSeparator)
+  return new Array(repeatTimes).fill(`${str}${additionString}`).join(separator)
 };
-  
