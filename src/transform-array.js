1bb93275ae4be7f6198ce0ff0801a52f0d0b3e62
Egor Nedelko <gojiramedia@gmail.com>
2021-01-20 11:56:54 +0300

    feat: transform-array-2 fails

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,39 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!arr instanceof Array) {
+     throw new Error
+  }
+   
+  let toDouble = false
+  let resultArr = []  
+  for (let i = 0; i < arr.length; i++) {
+     switch(arr[i]) {
+        case '--discard-next':
+            i++
+            break;
+        case '--discard-prev':
+            if (resultArr.length) {
+               resultArr.pop()
+            }
+            break;
+        case '--double-next':
+           toDouble = true
+           break;
+        case '--double-prev':
+           if (resultArr.length) {
+              resultArr.push(arr[i-1])
+           }
+           break;
+        default:
+           if (toDouble) {
+            resultArr.push(arr[i])
+            resultArr.push(arr[i])
+            toDouble = false
+           }
+           resultArr.push(arr[i])
+           break;
+     }
+  }
+  return resultArr
 };
