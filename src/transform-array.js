32c03fc3dabc41c74cd6a946ac0c57913d44b5a6
Egor Nedelko <gojiramedia@gmail.com>
2021-01-20 12:18:19 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,38 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  if (!arr instanceof Array) {
+  if (!Array.isArray(arr)) {
      throw new Error
   }
    
-  let toDouble = false
-  let resultArr = []  
+  let resultArr = []   
   for (let i = 0; i < arr.length; i++) {
      switch(arr[i]) {
         case '--discard-next':
             i++
             break;
         case '--discard-prev':
-            if (resultArr.length) {
+            if (resultArr.length !== 0 && arr[i - 2] !== '--discard-next') {
                resultArr.pop()
             }
             break;
         case '--double-next':
-           toDouble = true
+           if (i < arr.length - 1) {
+              resultArr.push(arr[i+1])
+           }
            break;
         case '--double-prev':
-           if (resultArr.length) {
+           if (i !== 0 && arr[i - 2] !== '--discard-next') {
               resultArr.push(arr[i-1])
            }
            break;
         default:
-           if (toDouble) {
-            resultArr.push(arr[i])
-            resultArr.push(arr[i])
-            toDouble = false
-           }
            resultArr.push(arr[i])
-           break;
      }
   }
   return resultArr
